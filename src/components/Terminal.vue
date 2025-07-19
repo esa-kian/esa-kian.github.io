d6502fb9877995aa947ec049cc968130e556ad84
Esa <ehsan_amd72@hotmail.com>
2025-07-19 20:31:01 +0300

    put links in anchor tag

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -17,9 +17,9 @@
             Copyleft (ɔ) 2025 Esa Kian · <a href="https://www.gnu.org/licenses/gpl-3.0.html">GPL-3.0</a>
         </span>
         <pre class="bio-text">
-        <span v-for="(line, index) in typedLines" :key="index">{{ line }}</span>
-        <iframe v-if="showPlayer" style="border-radius:12px" src="https://open.spotify.com/embed/track/2kRFrWaLWiKq48YYVdGcm8?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      </pre>
+            <span v-for="(line, index) in typedLines" :key="index" v-html="line"></span>
+            <iframe v-if="showPlayer" style="border-radius:12px" src="https://open.spotify.com/embed/track/2kRFrWaLWiKq48YYVdGcm8?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </pre>
     </div>
 </template>
 
@@ -33,7 +33,7 @@ const bioLines = [
     "currently executing from finland",
     "writes code with the calm precision of a hitman — silent, effective, and occasionally poetic",
     "> uptime",
-    "8 years deep in the software trenches", 
+    "8 years deep in the software trenches",
     "refactored legacy horrors",
     "turned business logic into something that almost resembles art",
     "doesn’t build software to impress recruiters",
@@ -44,7 +44,7 @@ const bioLines = [
     "thinks dirty",
     "automates everything",
     "> find / -name esa-kian",
-    "github. linkedin. medium.",
+    `<a href="https://github.com/esa-kian" target="_blank">github</a> · <a href="https://linkedin.com/in/esa-kian/" target="_blank">linkedin</a> · <a href="https://medium.com/@esa-kian" target="_blank">medium</a>`,
     "ping me via pull request or summon me in a failing production log",
     "> soundtrack",
     "listen to my favorite song while you pretend to read this:",
@@ -66,7 +66,7 @@ onMounted(async () => {
         }
         typedLines.value.push('')
     }
-    showPlayer.value = true; 
+    showPlayer.value = true;
 
 })
 </script>
@@ -109,6 +109,27 @@ onMounted(async () => {
     line-height: 1.6;
     margin-bottom: 0.2rem;
 }
+
+a {
+    color: #00ff00;
+    text-decoration: underline;
+}
+
+a:link {
+    color: #00ff00;
+    text-decoration: underline;
+}
+
+a:active {
+    color: #00ff00;
+    text-decoration: underline;
+}
+
+a:hover {
+    color: #00ff00;
+    text-decoration: underline;
+}
+
 
 @media (max-width: 600px) {
     .ascii-name {
